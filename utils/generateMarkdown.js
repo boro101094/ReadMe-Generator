027ef3d55f6949fa663726e3dcb90b93b736f8cd
Boris Cabrera <boro10194@gmail.com>
2023-05-13 23:12:28 -0400

    In this commit is created all the code for generating a ReadMe File index.js content:    - this file contains all the questions the user is going to answer.    - function to create the ReadMe file.    -init funcion where the app is going to run.
    generateMardown.js content:
       -it has all the logic to generate the readme file.

---
+++ utils/generateMarkdown.js
@@ -0,0 +1,73 @@
+// Function that returns a license badge based on which license is passed in
+// If there is no license, it returns an empty string
+function renderLicenseBadge(license) {
+  if (license !== `None`) {
+    return `https://img.shields.io/badge/license-${license}-green)`;
+  } else {
+    return ``;
+  }
+}
+
+// Function that returns the license link
+// If there is no license, it returns an empty string
+function renderLicenseLink(license) {
+  if (license === `Apache2.0`) {
+    return `https://opensource.org/licenses/Apache-2.0`;
+  } else if (license === `MIT`) {
+    return `https://opensource.org/licenses/MIT`;
+  } else if (license === `AGPLv3`) {
+    return `https://www.gnu.org/licenses/agpl-3.0`;
+  } else return ``;
+}
+
+// Function that returns the license section of README
+// If there is no license, it returns an empty string
+function renderLicenseSection(license) {
+  if (license) {
+    return `[![Project license](${renderLicenseBadge(
+      license
+    )})](${renderLicenseLink(String(license))})`;
+  } else {
+    return ``;
+  }
+}
+
+// Function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.Title}
+## License
+${renderLicenseSection(data.License)}
+
+## Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contribution](#contribution)
+- [Testing](#testing)
+- [Contact Information](#contact-information)
+
+## Description
+${data.Description}
+
+## Installation
+${data.Installation}
+
+## Usage
+${data.Usage}
+
+## License
+${data.License}
+
+## Contribution
+${data.Contributing}
+
+## Testing
+${data.Test}
+
+## Contact Information
+- GitHub: [${data.Github}](https://github.com/${data.Github})
+- Email: [${data.Email}](mailto:${data.Email})
+`;
+}
+
+module.exports = generateMarkdown;
